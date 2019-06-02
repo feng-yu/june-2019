a61879a832d03d961c56aea40a177ff4ed1eeccb
feng.yu <f1768019@gmail.com>
2019-06-01 22:57:12 -0400

    add solution for problem for June 1st

--- june1.py
+++ june1.py
@@ -14,5 +14,24 @@ Uniformly scatter a given number of points over the square
 Count the number of points inside the quadrant, i.e. having a distance from the origin of less than 1
 The ratio of the inside-count and the total-sample-count is an estimate of the ratio of the two areas,
 π/4. Multiply the result by 4 to estimate π.
-
 """
+import random
+
+def find_pi(N):
+    in_circle_count = 0;
+    for _ in range(N):
+        x = random.random()
+        y = random.random()
+        if x**2 + y**2 < 1:
+            in_circle_count += 1
+    pi = in_circle_count * 1000 // N * 4 / 1000  #Estimate π to 3 decimal places
+    return pi
+
+
+def main():
+    for _ in range(20):
+        N = 500000
+        pi = find_pi(N)
+        print(pi)
+
+main()
