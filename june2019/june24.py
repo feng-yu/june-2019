c4f5d2bc60412b2c1122a7df680fb87213ab4d69
feng.yu <f1768019@gmail.com>
2019-06-24 21:06:47 -0400

    add frozenset in the solution. return a set of frozenset

--- june2019/june24.py
+++ june2019/june24.py
@@ -23,8 +23,29 @@ def power_set(s):
                 result.append(n_list)
     return result
 
+
+def power_set1(s):
+    li = list(s)
+    result = [[]]
+    if not li:
+        return result
+    for index, element in enumerate(li):
+        si_li = [element]
+        result.append(si_li)
+        inter_result = power_set(li[index+1:])
+        for inter_ele in inter_result:
+            n_list = si_li + inter_ele
+            if n_list not in result:
+                result.append(n_list)
+
+    s_result = set()
+    for e in result:
+        s_result.add(frozenset(e))
+    return s_result
+
+
 data = [1, 2, 3]
-print(power_set(data))
+print(power_set1(data))
 
 
 
